4fdd3a3e64c9dd289b85a25833dbb07b7b484748
Pallavi Rawat <me@rawatpallavi.com>
2021-03-14 00:01:52 +0530

    downgrade webpack to match the one in react libraries + make it work

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,19 @@ const isDevEnv = process.env.NODE_ENV === "development";
 module.exports = {
   mode: isDevEnv ? "development" : "production",
   entry: {
-    vendors: ["react", "react-dom", "@material-ui/core"]
+    commons: ["react", "react-dom", "@material-ui/core"]
   },
 
   output: {
     path: path.join(__dirname, "build"),
-    filename: "commons.js",
-    publicPath: "/",
-    library: "Commons"
+    filename: "[name].js",
+    library: "[name]"
   },
-
   plugins: [
     new webpack.DllPlugin({
       format: isDevEnv ? true : false,
       context: __dirname,
-      path: path.join(__dirname, "build", "manifest.json")
+      path: path.join(__dirname, "build", "[name]-manifest.json")
     })
   ]
 };
